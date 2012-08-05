d4eae50f116f7a3cbadb66ffef7273546c7e388d
Pascal Mathis <github@snapserv.net>
2012-08-05 14:52:28 +0200

    Fixed size calculation. Used 1000 instead of 1024...

--- includes/class.Functions.inc.php
+++ includes/class.Functions.inc.php
@@ -205,7 +205,7 @@
 			$units = array('B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB');
 			if($bytes === 0) return '0 ' . $units[0];
 			return number_format(@round(
-				$bytes / pow(1000, ($i = floor(log($bytes, 1000)))), $precision
+				$bytes / pow(1024, ($i = floor(log($bytes, 1024)))), $precision
 			), $precision) . ' ' . $units[$i];
 		}
 		
